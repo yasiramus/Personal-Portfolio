e4053a4cba59632c8bbaa36f28a3f9b9294e90aa
yasira <yasirayusif@gmail.com>
2022-07-12 17:58:37 -0700

    added footer

--- js/portfolio.js
+++ js/portfolio.js
@@ -3,3 +3,10 @@ window.addEventListener('scroll', function () {
     const header = document.querySelector('nav')
     header.classList.toggle('sticky', window.scrollY > 0 )
 })
+
+
+// get current year 
+const latestDate = document.getElementById("currentyear");
+const currentYear = new Date().getFullYear();
+    
+latestDate.textContent = currentYear;
